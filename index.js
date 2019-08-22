f39879082d40867a2f7f0b41710870a0a0d6d0f6
zy <584450903@qq.com>
2019-08-22 20:40:54 +0800

    Signed-off-by: zy <584450903@qq.com>

--- index.js
+++ index.js
@@ -95,8 +95,9 @@ export default class PickImgCrop {
                 width: 2000//原图片宽
             }
         ];*/
-        return PickImgCroper.openPicker(options).then(res=>{
-            return options.multiple ? res : res[0];
-        });
+        return PickImgCroper.openPicker(options);
+        //     .then(res=>{
+        //     return options.multiple ? res : res[0];
+        // });
     }
 }
